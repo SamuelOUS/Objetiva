bd5faf50e44fe74782f8baacd5203395f0fde6b9
SamuelOU <samuelosur2004@gmail.com>
2026-08-17 20:00:54 -0500

    ✨ feat: implementar consultas QUERY de tareas

--- src/routes/tarea.routes.ts
+++ src/routes/tarea.routes.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 
-import { 
-    
-    crearTareaController,
-    obtenerTareasController,
-    obtenerTareaPorIdController,
-    actualizarTareaController,
-    eliminarTareaController
-} 
-
-from "../controllers/tarea.controller.js";
+import {
+  crearTareaController,
+  obtenerTareasController,
+  obtenerTareaPorIdController,
+  actualizarTareaController,
+  eliminarTareaController,
+  consultarTareasQueryController,
+} from "../controllers/tarea.controller.js";
 
 const router = Router();
 
@@ -18,5 +16,6 @@ router.get("/", obtenerTareasController);
 router.get("/:id", obtenerTareaPorIdController);
 router.put("/:id", actualizarTareaController);
 router.delete("/:id", eliminarTareaController);
+(router.query as any)("/", consultarTareasQueryController);
 
 export default router;
